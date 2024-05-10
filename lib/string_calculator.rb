0b941de5b5d133a97b6da1737e24d087ea09e44e
Avinashkrmehta <avi4791@gmail.com>
2024-05-10 21:09:48 +0530

    added logic to class method and created specs

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,5 +5,18 @@ class StringCalculator
   MAX_VALUE = 1000
 
   def add(numbers)
+    numbers.strip!
+
+    return 0 if numbers.empty?
+
+    match = numbers.match(/\/\/(.*?)\n(.*)/)
+
+    delimiter = match ? match[1] : DELIMITER
+
+    split_numbers = numbers.split(/#{delimiter}|\n/)
+    negative_numbers = split_numbers.select { |num| num.to_i.negative? }
+    raise NegativesNotAllowed.new("Negatives Not Allowed: #{negative_numbers.join(', ')}") unless negative_numbers.empty?
+
+    split_numbers.select { |num| num.to_i <= MAX_VALUE && num.to_i >= 0 }.map(&:to_i).reduce(0, &:+)
   end
 end
---
+++ specs/string_calculator_specs.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/string_calculator'
+require_relative '../lib/negatives_not_allowed'
+describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+end
